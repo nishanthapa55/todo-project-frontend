c4cddae1ad4227c957adee753b3ac2a907c60e76
nishanthapa55 <t3thni00@students.oamk.fi>
2024-03-16 21:09:34 +0200

    update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const BACKEND_ROOT_URL = 'http://localhost:3001';
+//const BACKEND_ROOT_URL = 'http://localhost:3001';
+const BACKEND_ROOT_URL = 'https://todo-project-backend-9i6p.onrender.com';
 import { Todos } from './class/Todo.js';
 
 const todos = new Todos(BACKEND_ROOT_URL);
